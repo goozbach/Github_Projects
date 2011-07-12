ce7b1835c67ff53c261238d65114413cd55e17fa
Derek Carter <goozbach@friocorte.com>
2011-07-12 17:04:50 -0400

    more stuff

--- generate_github_projects.pl
+++ generate_github_projects.pl
@@ -5,7 +5,10 @@ use warnings;
 
 use Config::Tiny;
 use Data::Dumper;
+use LWP;
 
+# debug flag
+my $debug = 1;
 # Create a config
 my $Config = Config::Tiny->new();
 
@@ -13,8 +16,15 @@ my $Config = Config::Tiny->new();
 $Config = Config::Tiny->read( 'mainconfig.ini' );
 
 # Reading properties
+## debug
 my $root_config = $Config->{_};
 my $github_config = $Config->{github};
+## for reals
+my $github_api_url = $Config->{'github'}->{'api_url'};
 
-print Dumper $root_config;
-print Dumper $github_config;
+my $ua = LWP::UserAgent->new();
+#$ua->credentials($out_uri->host() . ":" . $out_uri->port(), 'CORAID Control Node', $user, $pass);
+
+# debug and main loop
+print Dumper $root_config if $debug;
+print Dumper $github_api_url if $debug;
